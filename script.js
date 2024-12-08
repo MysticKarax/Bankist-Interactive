908913e0c520a98576a4391613ccfe0596776898
Jordan Garcia <jordidev@proton.me>
2024-12-08 17:02:43 -0600

    added(HTML, CSS, JS): Sticky Header on features, main sections IDs

--- script.js
+++ script.js
@@ -1,9 +1,72 @@
-const hamburguerMenu = document.querySelector(
+// HAMBURGUER MENU
+
+const H_HAMBURGUER_MENU = document.querySelector(
   ".header__hamburguer-menu-toggle-label"
 );
 
 console.log("object");
 
-hamburguerMenu.addEventListener("click", () => {
-  hamburguerMenu.classList.toggle("active");
+H_HAMBURGUER_MENU.addEventListener("click", () => {
+  H_HAMBURGUER_MENU.classList.toggle("active");
 });
+
+const formElement = document.getElementById("form__dialog");
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault(); // Previene el comportamiento por defecto del formulario
+  console.log("Formulario no enviado, acción personalizada aquí.");
+});
+
+// MODAL WINDOW
+
+const H_DIALOG = document.querySelector("dialog");
+console.log(H_DIALOG);
+const H_BTN_CLOSE_MODAL = document.querySelector(".btn-dialog__close");
+console.log(H_BTN_CLOSE_MODAL);
+const H_BTN_SIGNUP = document.querySelectorAll(".signup-btn");
+console.log(H_BTN_SIGNUP);
+
+// SI DEJA DE SER UN DIALOGO, ENTONCES AVISARLO
+if (!(H_DIALOG instanceof HTMLDialogElement)) {
+  console.error("Modal Window Not Found");
+  throw new Error();
+}
+
+// POR CADA BOTON CON LA CLASE "SIGNUP" QUE HAYA, AGREGAR UN EVENT
+// LISTENER Y ABRIR LA MODAL CUANDO SE HAGA CLICK EN CUALQUIERA
+// DE ELLOS
+H_BTN_SIGNUP.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    H_DIALOG.showModal();
+    console.log("Abriendo Modal");
+  })
+);
+
+// AL DAR CLICK EN LA X, CERRAR EL MODAL
+H_BTN_CLOSE_MODAL.addEventListener("click", () => {
+  H_DIALOG.close();
+  console.log("Boton Cerrar Modal");
+});
+
+// AL DAR CLICK FUERA DEL MODAL, CERRAR EL MODAL
+H_DIALOG.addEventListener("click", (e) => {
+  if (e.target === H_DIALOG) {
+    H_DIALOG.close();
+    console.log("Click en Overlay");
+  }
+});
+
+// ACTIVANDO STICKY HEADER
+
+window.onscroll = function () {
+  const H_HEADER = document.querySelector("header");
+  const H_FEATURES_SECTION = document.querySelector(".features");
+  const H_SECTION_TOP = H_FEATURES_SECTION.offsetTop;
+
+  if (window.scrollY >= H_SECTION_TOP) {
+    H_HEADER.classList.add("sticky");
+  } else {
+    H_HEADER.classList.remove("sticky");
+  }
+};
